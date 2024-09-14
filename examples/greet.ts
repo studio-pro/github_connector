fc1a8b7655a0b15c934e8339dfd935ce390a359d
studio-pro <studio@wrtn.io>
2024-09-15 00:52:51 +0900

    Add greet function to new file

---
+++ examples/greet.ts
@@ -0,0 +1,10 @@
+/**
+ * Greets the user with a message.
+ * This function is intended to demonstrate TypeScript function annotations.
+ *
+ * @param name - The name of the user to greet.
+ * @returns A greeting message string.
+ */
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
