33382e04436eeb72e2e545ae904355e0be7420ff
studio-pro <studio@wrtn.io>
2024-09-15 00:53:12 +0900

    Add splitSentence function to new file

---
+++ examples/splitSentence.ts
@@ -0,0 +1,9 @@
+/**
+ * Splits a sentence into words.
+ * 
+ * @param sentence - A string of text to split.
+ * @returns An array of words.
+ */
+function splitSentence(sentence: string): string[] {
+  return sentence.split(' ');
+}
